94da4d6b85f5329716f184212d78a89254f14f4b
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-01-04 22:38:00 +0530

    update

---
+++ Searching Techniques/Binary Search/Ceiling.java
@@ -0,0 +1,28 @@
+public class Ceiling {
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,7,9,24,25,30};
+        System.out.println(ceiling(arr,8 ));
+    }
+
+    static int ceiling(int[] arr, int target){
+
+        int start = 0;
+        int end = arr.length -1;
+        int mid = start + (end - start)/2;
+        
+
+        while(start<=end){
+            if(arr[mid]>= target){
+                end = mid -1;
+            }else if(arr[mid]<= target){
+                start = mid + 1;
+            }else if(arr[mid] == target){
+                
+                return arr[mid];
+            }
+        }
+        return start;
+    }
+    
+}
