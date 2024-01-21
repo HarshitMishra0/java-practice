07f28626a4b07556b3eea71ed3869c9082660a8d
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-01-21 23:42:52 +0530

    update

---
+++ Recursion/Fibo.java
@@ -0,0 +1,19 @@
+
+public class Fibo {
+    public static void main(String[] args) {
+        
+        int ans = fibonacci(6);
+
+        System.out.println(ans);
+    }
+
+    static int fibonacci(int n){
+
+        if(n<2){
+            return n;
+        }
+
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+    
+}
